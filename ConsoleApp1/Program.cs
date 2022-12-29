8727036195772baab416b69a7444f43717e05fc5
Ihor <igorekjjjj@gmail.com>
2022-12-29 19:16:04 +0200

    LABA 3

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            View view;
+            view = new View();
+            while (true)
+            {
+                view.viewMenu();
+                Console.WriteLine();
+                Console.WriteLine("Вийти з програми? '+'-Yes | '-'-No");
+                if (Console.ReadLine() == "+")
+                    break;
+                Console.WriteLine();
+            }
+        }
+    }
+}
